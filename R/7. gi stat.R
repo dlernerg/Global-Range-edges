4f134bb0fc7efe7214dea9ace4efef4fac2e1a1a
dlernerg <101254508+dlernerg@users.noreply.github.com>
2022-05-22 13:36:07 +0300

    Update 7. gi stat.R

--- R/7. gi stat.R
+++ R/7. gi stat.R
@@ -25,6 +25,7 @@ count.which <- counts.coord.inland2[complete.cases(counts.coord.inland2),]
 global.inland2 <- global.inland[complete.cases(counts.coord.inland2),]
 st_geometry(count.which) <- st_geometry(global.inland2)
 
+#find the neighbours of each of the grids of the rastered world. Important to use include.self 
 neighbours <- poly2nb(count.which, queen = F)
 listw <- nb2listw(include.self(neighbours), style = "W", zero.policy = TRUE)
 coords <- c("N","S","E","W")
@@ -32,19 +33,12 @@ coords <- c("N","S","E","W")
 local_G <- data.frame()
 for (i in 1:4){
   a <- count.which
-  #globalMoran <- moran.test(a$N,listw)
-  #moran <- moran.plot(a$all, listw = nb2listw(neighbours, style = "W", zero.policy = TRUE))
-  #local <- localmoran(x = a$E, listw,zero.policy = TRUE) %>% as.data.frame()
-  #local = local %>% 
-  #  mutate(quantilegroup = as.numeric(unique(dvmisc::create_qgroups(Ii, groups = 10))))
   a_c <- a[,c(i,5)] 
   names(a_c)[1] <- "coord"
   local_g <- as.matrix(localG(a_c$coord, listw)) 
   local_g <- as.data.frame(cbind(a_c$coord, as.vector(local_g)))
   names(local_g) <- c("cell","gstat")
-  #b_S <- cbind(global.inland,local_g)
-  
-  #local_g$bins <- (cut(local_g$gstat, breaks=c(-1,3,7,11,13)))
+ 
   local_g$coord <- coords[i]
   
   local_G <- rbind(local_G,local_g)
